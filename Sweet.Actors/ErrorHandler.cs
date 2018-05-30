2b2c2d9de324d5cd4649942fd5fa93eaebb404e5
Cagatay Dogan <ocdogan@gmail.com>
2018-05-30 09:06:26 +0300

    Error handler implementation

---
+++ Sweet.Actors/ErrorHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sweet.Actors
+{
+    public interface IErrorHandler
+    {
+        void HandleError(IProcess process, IMessage msg, Exception error);
+    }
+
+    internal class DefaultErrorHandler : IErrorHandler
+    {
+        public static readonly IErrorHandler Instance = new DefaultErrorHandler();
+
+        public void HandleError(IProcess process, IMessage msg, Exception error)
+        {
+        }
+    }
+}
